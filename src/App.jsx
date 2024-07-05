e2a8e47c9ce5ab34a36a2a92330a2ce8cc6ed7bf
Adão Araujo <adaoma11@hotmail.com>
2024-07-05 16:03:21 -0300

    feat: add game stages screen components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import StartScreen from "./components/StartScreen";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
+// components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
